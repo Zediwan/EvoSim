859d79230c91181ea5e5cada1de819bfa17b8b9b
Jeremy Moser <jeremymoser@opacc.ch>
2025-04-25 13:22:39 +0200

    Refactor Organism class for improved structure
    The `Organism` class has been refactored to enhance readability and organization. Removed unnecessary `using` directives and simplified the namespace declaration. Introduced regions to group related variables, properties, constructors, and methods. Added health and energy management methods with input validation, and updated the constructor to ensure health and energy are initialized within defined limits.

--- EvoSim/Model/Organism.cs
+++ EvoSim/Model/Organism.cs
@@ -1,102 +1,128 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Automation;
+﻿namespace EvoSim.Model;
 
-namespace EvoSim.Model
+public class Organism
 {
-    public class Organism
+
+
+    #region Variables and Properties
+
+    #region Health Variables and Properties
+
+    internal int MaxHealth => 100;
+
+    private int _health;
+
+    public int Health => _health;
+
+    public bool IsAlive => Health > 0;
+
+    #endregion
+
+    #region Energy Variables and Properties
+
+    private int _energy;
+
+    internal int MaxEnergy => 100;
+
+    public int Energy => _energy;
+
+    #endregion
+
+    #endregion
+
+
+    #region Constructors and Destructors
+
+    public Organism(int startingHealth = 0, int startingEnergy = 0)
     {
-        internal int MaxHealth => 100;
+        if (startingHealth < 0)
+            throw new ArgumentOutOfRangeException(nameof(startingHealth), "Starting Health must be positive.");
+        if (startingHealth > MaxHealth)
+            throw new ArgumentOutOfRangeException(nameof(startingHealth),
+                $"Starting Health must be smaller or equal to {nameof(MaxEnergy)}: {MaxEnergy}");
+        if (startingEnergy < 0)
+            throw new ArgumentOutOfRangeException(nameof(startingEnergy), "Starting Energy must be positive.");
+        if (startingEnergy > MaxEnergy)
+            throw new ArgumentOutOfRangeException(nameof(startingEnergy),
+                $"Starting Energy must be smaller or equal to {nameof(MaxEnergy)}: {MaxEnergy}");
 
-        private int _health;
-
-        public int Health => _health;
-
-        public bool IsAlive => Health > 0;
-
-        private int _energy;
-
-        internal int MaxEnergy => 100;
-
-        public int Energy => _energy;
-
-
-        public Organism(int startingHealth = 0, int startingEnergy = 0)
-        {
-            if (startingHealth < 0)
-                throw new ArgumentOutOfRangeException(nameof(startingHealth), "Starting Health must be positive.");
-            if (startingHealth > MaxHealth)
-                throw new ArgumentOutOfRangeException(nameof(startingHealth),
-                    $"Starting Health must be smaller or equal to {nameof(MaxEnergy)}: {MaxEnergy}");
-            if (startingEnergy < 0)
-                throw new ArgumentOutOfRangeException(nameof(startingEnergy), "Starting Energy must be positive.");
-            if (startingEnergy > MaxEnergy)
-                throw new ArgumentOutOfRangeException(nameof(startingEnergy),
-                    $"Starting Energy must be smaller or equal to {nameof(MaxEnergy)}: {MaxEnergy}");
-
-            SetHealth(startingHealth);
-            SetEnergy(startingEnergy);
-        }
-
-        internal void SetHealth(int value)
-        {
-            if (value < 0)
-                throw new ArgumentOutOfRangeException(nameof(value), "Value must be positive.");
-            if (value > MaxHealth)
-                throw new ArgumentOutOfRangeException(nameof(value), $"Value must be smaller or equal to {nameof(MaxHealth)}: {MaxHealth}");
-            _health = value;
-        }
-
-        public void TakeDamage(int amount)
-        {
-            if (amount <= 0)
-                throw new ArgumentOutOfRangeException(nameof(amount), "Damage must be positive.");
-
-            SetHealth(Math.Max(Health - amount, 0));
-        }
-
-        public void Heal(int amount)
-        {
-            if (amount <= 0)
-                throw new ArgumentOutOfRangeException(nameof(amount), "Heal amount must be positive.");
-
-            SetHealth(Math.Min(Health + amount, MaxHealth));
-        }
-
-        internal void SetEnergy(int value)
-        {
-            if (value < 0)
-                throw new ArgumentOutOfRangeException(nameof(value), "Value must be positive.");
-            if (value > MaxEnergy)
-                throw new ArgumentOutOfRangeException(nameof(value), $"Energy must be smaller or equal to {nameof(MaxEnergy)}: {MaxEnergy}.");
-            _energy = value;
-        }
-
-
-        public void UseEnergy(int amount)
-        {
-            if (amount <= 0)
-                throw new ArgumentOutOfRangeException(nameof(amount), "Energy used must be positive.");
-
-            var newEnergy = Energy - amount;
-
-            if (newEnergy <= 0)
-            {
-                TakeDamage(-newEnergy);
-            }
-
-            SetEnergy(Math.Max(newEnergy, 0));
-        }
-
-        public void GainEnergy(int amount)
-        {
-            if (amount <= 0)
-                throw new ArgumentOutOfRangeException(nameof(amount), "Energy gained must be positive.");
-
-            SetEnergy(Math.Min(Energy + amount, MaxEnergy));
-        }
+        SetHealth(startingHealth);
+        SetEnergy(startingEnergy);
     }
+
+    #endregion
+
+
+    #region Methods
+
+    #region Health
+
+    internal void SetHealth(int value)
+    {
+        if (value < 0)
+            throw new ArgumentOutOfRangeException(nameof(value), "Value must be positive.");
+        if (value > MaxHealth)
+            throw new ArgumentOutOfRangeException(nameof(value), $"Value must be smaller or equal to {nameof(MaxHealth)}: {MaxHealth}");
+        _health = value;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(amount), "Damage must be positive.");
+
+        SetHealth(Math.Max(Health - amount, 0));
+    }
+
+    public void Heal(int amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(amount), "Heal amount must be positive.");
+
+        SetHealth(Math.Min(Health + amount, MaxHealth));
+    }
+
+
+    #endregion
+
+    #region Energy
+
+    internal void SetEnergy(int value)
+    {
+        if (value < 0)
+            throw new ArgumentOutOfRangeException(nameof(value), "Value must be positive.");
+        if (value > MaxEnergy)
+            throw new ArgumentOutOfRangeException(nameof(value), $"Energy must be smaller or equal to {nameof(MaxEnergy)}: {MaxEnergy}.");
+        _energy = value;
+    }
+
+    public void UseEnergy(int amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(amount), "Energy used must be positive.");
+
+        var newEnergy = Energy - amount;
+
+        if (newEnergy <= 0)
+        {
+            TakeDamage(-newEnergy);
+        }
+
+        SetEnergy(Math.Max(newEnergy, 0));
+    }
+
+    public void GainEnergy(int amount)
+    {
+        if (amount <= 0)
+            throw new ArgumentOutOfRangeException(nameof(amount), "Energy gained must be positive.");
+
+        SetEnergy(Math.Min(Energy + amount, MaxEnergy));
+    }
+
+    #endregion
+
+    #endregion
+
+
 }
+

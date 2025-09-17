c3d20cfdd4006f8707bce520feb8e40f85828232
Jeremy Moser <jeremymoser@opacc.ch>
2025-09-17 13:30:20 +0200

    Added Property Comments to Color Component

--- EvoSim/ECS/Components/ColorComponent.cs
+++ EvoSim/ECS/Components/ColorComponent.cs
@@ -4,8 +4,20 @@ namespace EvoSim.ECS.Components;
 
 public class ColorComponent : IComponent
 {
+    /// <summary>
+    /// Gets or sets the red component of the color, represented as a value between 0 and 255..
+    /// </summary>
     public byte R { get; set; }
+    /// <summary>
+    /// Gets or sets the green component of a color, represented as a value between 0 and 255.
+    /// </summary>
     public byte G { get; set; }
+    /// <summary>
+    /// Gets or sets the value of the byte property, represented as a value between 0 and 255..
+    /// </summary>
     public byte B { get; set; }
+    /// <summary>
+    /// Gets the color value as a 32-bit unsigned integer in ARGB format.
+    /// </summary>
     public uint ARGB => (uint)((255 << 24) | (R << 16) | (G << 8) | B);
 }

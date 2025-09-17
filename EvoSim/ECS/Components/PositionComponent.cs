a2edeab4945483d2482f6da8d3ac8c6dd3b9ba05
Jeremy Moser <jeremymoser@opacc.ch>
2025-09-17 13:31:35 +0200

    Added Property Comments to Position Component

--- EvoSim/ECS/Components/PositionComponent.cs
+++ EvoSim/ECS/Components/PositionComponent.cs
@@ -4,6 +4,12 @@ namespace EvoSim.ECS.Components;
 
 public class PositionComponent : IComponent
 {
+    /// <summary>
+    /// Represents the X-coordinate value in a two-dimensional space.
+    /// </summary>
     public int X;
+    /// <summary>
+    /// Represents the Y-coordinate of a point or object in a two-dimensional space.
+    /// </summary>
     public int Y;
 }

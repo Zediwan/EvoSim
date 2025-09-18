ad76fe6b521774cc06f14b31ed9ee6f5b3d292b9
Jeremy Moser <jeremymoser@opacc.ch>
2025-09-18 08:32:21 +0200

    Created primary constructor for Systems

--- EvoSim/ECS/Systems/EnergySystem.cs
+++ EvoSim/ECS/Systems/EnergySystem.cs
@@ -6,23 +6,16 @@ using EvoSim.ECS.Utilities;
 
 namespace EvoSim.ECS.Systems;
 
-public class EnergySystem : ISystem
+public class EnergySystem(int drainRate = 1) : ISystem
 {
-    private readonly int _drainRate;
-
-    public EnergySystem(int drainRate = 1)
-    {
-        _drainRate = drainRate;
-    }
-
     public void Update(EcsEngine ecsEngine, float deltaTime)
     {
         Debug.Assert(deltaTime >= 0, $"Delta time ({deltaTime}) cannot be negative.");
-        Debug.Assert(_drainRate >= 0, $"Drain rate ({_drainRate}) cannot be negative.");
+        Debug.Assert(drainRate >= 0, $"Drain rate ({drainRate}) cannot be negative.");
 
         foreach (var entity in ecsEngine.GetEntitiesWith<EnergyComponent>())
         {
-            EnergyUtility.UseEnergy(entity, (_drainRate * deltaTime));
+            EnergyUtility.UseEnergy(entity, (drainRate * deltaTime));
         }
     }
 }
--- EvoSim/ECS/Systems/PositionSystem.cs
+++ EvoSim/ECS/Systems/PositionSystem.cs
@@ -4,22 +4,13 @@ using EvoSim.ECS.Utilities;
 
 namespace EvoSim.ECS.Systems;
 
-public class PositionSystem
+public class PositionSystem(int width, int height) : ISystem
 {
-    private readonly int _width;
-    private readonly int _height;
-
-    public PositionSystem(int width, int height)
-    {
-        _width = width;
-        _height = height;
-    }
-
     public void Update(EcsEngine world, float deltaTime)
     {
         foreach (var entity in world.GetEntitiesWith(typeof(PositionComponent)))
         {
-            PositionUtility.CalculateWraparoundPosition(entity.GetComponent<PositionComponent>(), _width, _height);
+            PositionUtility.CalculateWraparoundPosition(entity.GetComponent<PositionComponent>(), width, height);
         }
     }
 }

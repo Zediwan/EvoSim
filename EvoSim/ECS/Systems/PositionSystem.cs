34042284c8c8457554dbf6468c5fd2700799f5c4
Jeremy Moser <jeremymoser@opacc.ch>
2025-09-18 08:27:36 +0200

    Refactored Wraparound responsibility to PositionSystem

---
+++ EvoSim/ECS/Systems/PositionSystem.cs
@@ -0,0 +1,25 @@
+﻿using EvoSim.ECS.Components;
+using EvoSim.ECS.Core;
+using EvoSim.ECS.Utilities;
+
+namespace EvoSim.ECS.Systems;
+
+public class PositionSystem
+{
+    private readonly int _width;
+    private readonly int _height;
+
+    public PositionSystem(int width, int height)
+    {
+        _width = width;
+        _height = height;
+    }
+
+    public void Update(EcsEngine world, float deltaTime)
+    {
+        foreach (var entity in world.GetEntitiesWith(typeof(PositionComponent)))
+        {
+            PositionUtility.CalculateWraparoundPosition(entity.GetComponent<PositionComponent>(), _width, _height);
+        }
+    }
+}
--- EvoSim/ECS/Systems/VelocitySystem.cs
+++ EvoSim/ECS/Systems/VelocitySystem.cs
@@ -6,20 +6,11 @@ namespace EvoSim.ECS.Systems;
 
 public class VelocitySystem : ISystem
 {
-    private readonly int _width;
-    private readonly int _height;
-
-    public VelocitySystem(int width, int height)
-    {
-        _width = width;
-        _height = height;
-    }
-
     public void Update(EcsEngine world, float deltaTime)
     {
         foreach (var entity in world.GetEntitiesWith(typeof(VelocityComponent), typeof(PositionComponent)))
         {
-            VelocityUtility.ApplyVelocityToPosition(entity, _width, _height);
+            VelocityUtility.ApplyVelocityToPosition(entity);
         }
     }
 }

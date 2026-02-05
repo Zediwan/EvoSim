7057f682463c17581d380fea2b1b837e09b9ff5c
Jeremy Moser <jeremymoser@opacc.ch>
2026-02-05 10:47:30 +0100

    Added random rotational acceleration

--- EvoSim/ECS/Systems/AccelerationSystem.cs
+++ EvoSim/ECS/Systems/AccelerationSystem.cs
@@ -6,12 +6,17 @@ namespace EvoSim.ECS.Systems;
 
 public class AccelerationSystem : ISystem
 {
+    private const int MAX_ROTATION_ANGLE = 45; // degrees
+
     public void Update(EcsEngine ecsEngine, float deltaTime)
     {
         foreach (var entity in ecsEngine.GetEntitiesWith<AccelerationComponent>())
         {
+            var accelerationComponent = entity.GetComponent<AccelerationComponent>();
+            var (rotX, rotY) = VectorUtility.GetRotationVector(accelerationComponent.AX, accelerationComponent.AY,
+                Random.Shared.NextDouble() * MAX_ROTATION_ANGLE * 2 - MAX_ROTATION_ANGLE);
             // Randomly change acceleration
-            AccelerationUtility.ApplyAcceleration(entity, (float)(Random.Shared.NextDouble() * 2 - 1), (float)(Random.Shared.NextDouble() * 2 - 1), deltaTime);
+            AccelerationUtility.ApplyAcceleration(entity, rotX, rotY, deltaTime);
             // Apply acceleration to velocity
             AccelerationUtility.ApplyAccelerationToVelocity(entity, deltaTime);
         }

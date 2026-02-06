df2beb7cafd582b057644a1e77b7a2db327c5e5d
Jeremy Moser <jeremymoser@opacc.ch>
2026-02-06 10:50:43 +0100

    Closes #115 Combat: Added new Utility Method to calculate Damage an attacker deals to an defender

---
+++ EvoSim.Test/ECS/Utilities/CombatUtilityTest.cs
@@ -0,0 +1,91 @@
+﻿using EvoSim.ECS.Components;
+using EvoSim.ECS.Core;
+using EvoSim.ECS.Utilities;
+
+namespace EvoSim.Test.ECS.Utilities;
+
+public class CombatUtilityTest
+{
+    public class CalculateDamageDealtTest()
+    {
+        [Theory]
+        [InlineData(10, 5, 5)]
+        [InlineData(10, 0, 10)]
+        public void Should_Hit_When_MoreAttackThanDefense(float attack, float defense, float expected)
+        {
+            // Arrange
+            var ecsEngine = new EcsEngine();
+
+            var attacker = ecsEngine.CreateEntity();
+            attacker.AddComponent(new CombatComponent { Attack = attack });
+
+            var defender = ecsEngine.CreateEntity();
+            defender.AddComponent(new CombatComponent { Defense = defense });
+
+            // Act
+            var result = CombatUtility.CalculateDamageDealt(attacker, defender);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData(5, 5)]
+        [InlineData(0, 10)]
+        public void Should_NotHit_When_LessOrEqualAttackThanDefense(float attack, float defense)
+        {
+            // Arrange
+            var ecsEngine = new EcsEngine();
+
+            var attacker = ecsEngine.CreateEntity();
+            attacker.AddComponent(new CombatComponent { Attack = attack });
+
+            var defender = ecsEngine.CreateEntity();
+            defender.AddComponent(new CombatComponent { Defense = defense });
+
+            // Act
+            var result = CombatUtility.CalculateDamageDealt(attacker, defender);
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Should_NotHit_When_AttackerNoCombatComponent()
+        {
+            // Arrange
+            var ecsEngine = new EcsEngine();
+
+            var attacker = ecsEngine.CreateEntity();
+            // Attacker has no CombatComponent
+
+            var defender = ecsEngine.CreateEntity();
+            defender.AddComponent(new CombatComponent { Defense = 5 });
+
+            // Act
+            var result = CombatUtility.CalculateDamageDealt(attacker, defender);
+
+            // Assert
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        public void Should_AutoHit_When_DefenderNoCombatComponent()
+        {
+            // Arrange
+            var ecsEngine = new EcsEngine();
+
+            var attacker = ecsEngine.CreateEntity();
+            attacker.AddComponent(new CombatComponent { Attack = 10 });
+
+            var defender = ecsEngine.CreateEntity();
+            // Defender has no CombatComponent
+
+            // Act
+            var result = CombatUtility.CalculateDamageDealt(attacker, defender);
+
+            // Assert
+            Assert.Equal(10, result);
+        }
+    }
+}

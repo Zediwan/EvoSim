2901b8252860229600c7395a9ac2237540f790ac
Jeremy Moser <jeremymoser@opacc.ch>
2026-02-10 13:58:22 +0100

    Energy: Reworked Energy Component Tests

--- EvoSim.Test/ECS/Components/EnergyComponentTest.cs
+++ EvoSim.Test/ECS/Components/EnergyComponentTest.cs
@@ -4,91 +4,25 @@ namespace EvoSim.Test.ECS.Components;
 
 public class EnergyComponentTest
 {
-    public class ConstructorTests
+    [Theory]
+    [InlineData(10.0f, 20.0f, -5.0f, null, 0.0f, 20.0f)]
+    [InlineData(10.0f, 20.0f, 25.0f, null, 20.0f, 20.0f)]
+    [InlineData(10.0f, 20.0f, null, -5.0f, 0.0f, 0.0f)]
+    [InlineData(10.0f, 20.0f, null, 5.0f, 5.0f, 5.0f)]
+    [InlineData(10.0f, 20.0f, 15.0f, 25.0f, 15.0f, 25.0f)]
+    [InlineData(10.0f, 20.0f, null, null, 10.0f, 20.0f)]
+    public void MaxEnergyTest(float initialEnergy, float initialMaxEnergy, float? newEnergy, float? newMaxEnergy,
+        float expectedEnergy, float expectedMaxEnergy)
     {
-        [Fact]
-        public void Should_InitializeWithCorrectValues()
-        {
-            // Arrange
-            const float energy = 10;
-            const float maxEnergy = 20;
+        // Arrange
+        var energyComponent = new EnergyComponent(maxEnergy: initialMaxEnergy, energy: initialEnergy);
 
-            // Act
-            var energyComponent = new EnergyComponent(maxEnergy: maxEnergy, energy: energy);
+        // Act
+        if (newEnergy.HasValue) energyComponent.Energy = newEnergy.Value;
+        if (newMaxEnergy.HasValue) energyComponent.MaxEnergy = newMaxEnergy.Value;
 
-            // Assert
-            Assert.Equal(energy, energyComponent.Energy);
-            Assert.Equal(maxEnergy, energyComponent.MaxEnergy);
-        }
-
-        public class ReleaseTests : ReleaseTest
-        {
-            // Empty for future extension
-        }
-    }
-
-    public class SetterTests
-    {
-        [Fact]
-        public void Should_AllowUpdatingValues()
-        {
-            // Arrange
-            var energyComponent = new EnergyComponent(maxEnergy: 20, energy: 10);
-            // Act
-            energyComponent.MaxEnergy = 25;
-            energyComponent.Energy = 15;
-            // Assert
-            Assert.Equal(15, energyComponent.Energy);
-            Assert.Equal(25, energyComponent.MaxEnergy);
-        }
-
-        [Fact]
-        public void Should_ClampEnergy_When_MaxEnergySmallerThanEnergy()
-        {
-            // Arrange
-            var energyComponent = new EnergyComponent(maxEnergy: 20, energy: 10);
-
-            // Act
-            energyComponent.MaxEnergy = 5;
-
-            // Assert
-            Assert.Equal(energyComponent.MaxEnergy, energyComponent.Energy);
-        }
-
-        public class ReleaseTests : ReleaseTest
-        {
-            [SkippableFact]
-            public void Should_ClampEnergy_WhenSettingNegativeEnergy()
-            {
-                // Arrange
-                var energyComponent = new EnergyComponent(maxEnergy: 20, energy: 10);
-                // Act
-                energyComponent.Energy = -5;
-                // Assert
-                Assert.Equal(0, energyComponent.Energy);
-            }
-
-            [SkippableFact]
-            public void Should_ClampEnergy_WhenSettingEnergyGreaterThanMaxEnergy()
-            {
-                // Arrange
-                var energyComponent = new EnergyComponent(maxEnergy: 20, energy: 10);
-                // Act
-                energyComponent.Energy = 25;
-                // Assert
-                Assert.Equal(energyComponent.MaxEnergy, energyComponent.Energy);
-            }
-
-            [SkippableFact]
-            public void Should_ClampMaxEnergy_WhenSettingNegativeMaxEnergy()
-            {
-                // Arrange
-                var energyComponent = new EnergyComponent(maxEnergy: 20, energy: 10);
-                // Act
-                energyComponent.MaxEnergy = -5;
-                // Assert
-                Assert.Equal(0, energyComponent.MaxEnergy);
-            }
-        }
+        // Assert
+        Assert.Equal(expectedEnergy, energyComponent.Energy);
+        Assert.Equal(expectedMaxEnergy, energyComponent.MaxEnergy);
     }
 }

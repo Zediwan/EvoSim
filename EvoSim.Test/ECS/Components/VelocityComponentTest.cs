54c571557c93bb06c8ee25cec197bafd7a1f0b99
Jeremy Moser <jeremymoser@opacc.ch>
2026-02-11 07:10:40 +0100

    Velocity: Reworked Velocity component Tests

--- EvoSim.Test/ECS/Components/VelocityComponentTest.cs
+++ EvoSim.Test/ECS/Components/VelocityComponentTest.cs
@@ -5,10 +5,10 @@ namespace EvoSim.Test.ECS.Components;
 public class VelocityComponentTest
 {
     [Theory]
-    [InlineData(3, 4, 5)] // 3-4-5 triangle
-    [InlineData(5, 12, 13)] // 5-12-13 triangle
-    [InlineData(8, 15, 17)] // 8-15-17 triangle
-    [InlineData(0, 0, 0)] // Zero velocity
+    [InlineData(3.0f, 4.0f, 5.0f)] // 3-4-5 triangle
+    [InlineData(5.0f, 12.0f, 13.0f)] // 5-12-13 triangle
+    [InlineData(8.0f, 15.0f, 17.0f)] // 8-15-17 triangle
+    [InlineData(0.0f, 0.0f, 0.0f)] // Zero velocity
     public void TotalVelocityTest(float vx, float vy, float expectedTotalVelocity)
     {
         // Arrange
@@ -22,10 +22,10 @@ public class VelocityComponentTest
     }
 
     [Theory]
-    [InlineData(3, 4, 25)] // 3-4-5 triangle
-    [InlineData(5, 12, 169)] // 5-12-13 triangle
-    [InlineData(8, 15, 289)] // 8-15-17 triangle
-    [InlineData(0, 0, 0)] // Zero velocity
+    [InlineData(3.0f, 4.0f, 25.0f)] // 3-4-5 triangle
+    [InlineData(5.0f, 12.0f, 169.0f)] // 5-12-13 triangle
+    [InlineData(8.0f, 15.0f, 289.0f)] // 8-15-17 triangle
+    [InlineData(0.0f, 0.0f, 0.0f)] // Zero velocity
     public void TotalVelocitySquaredTests(float vx, float vy, float expectedTotalVelocitySquared)
     {
         // Arrange
@@ -39,20 +39,20 @@ public class VelocityComponentTest
     }
 
     [Theory]
-    [InlineData(0, 0, false)]
-    [InlineData(3, 4, true)]
-    [InlineData(-3, 4, true)]
-    [InlineData(3, -4, true)]
-    [InlineData(-3, -4, true)]
-    [InlineData(1, 0, true)]
-    [InlineData(-1, 0, true)]
-    [InlineData(2, 0, true)]
-    [InlineData(-2, 0, true)]
-    [InlineData(float.Epsilon, 0, true)]
-    [InlineData(0, float.Epsilon, true)]
+    [InlineData(0.0f, 0.0f, false)]
+    [InlineData(3.0f, 4.0f, true)]
+    [InlineData(-3.0f, 4.0f, true)]
+    [InlineData(3.0f, -4.0f, true)]
+    [InlineData(-3.0f, -4.0f, true)]
+    [InlineData(1.0f, 0.0f, true)]
+    [InlineData(-1.0f, 0.0f, true)]
+    [InlineData(2.0f, 0.0f, true)]
+    [InlineData(-2.0f, 0.0f, true)]
+    [InlineData(float.Epsilon, 0.0f, true)]
+    [InlineData(0.0f, float.Epsilon, true)]
     [InlineData(float.Epsilon, float.Epsilon, true)]
-    [InlineData(-float.Epsilon, 0, true)]
-    [InlineData(0, -float.Epsilon, true)]
+    [InlineData(-float.Epsilon, 0.0f, true)]
+    [InlineData(0.0f, -float.Epsilon, true)]
     [InlineData(-float.Epsilon, -float.Epsilon, true)]
 
     public void HasVelocityTest(float vx, float vy, bool shouldHaveVelocity)

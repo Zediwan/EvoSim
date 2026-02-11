1480cd371a44cc6ec286e9fa37cb1a84e859ccf7
Jeremy Moser <jeremymoser@opacc.ch>
2026-02-11 07:10:26 +0100

    Position: Reworked Position component Tests

--- EvoSim.Test/ECS/Components/PositionComponentTest.cs
+++ EvoSim.Test/ECS/Components/PositionComponentTest.cs
@@ -5,41 +5,22 @@ namespace EvoSim.Test.ECS.Components;
 
 public class PositionComponentTest
 {
-    [Fact]
-    public void PositionComponent_Initialization_Works()
+    [Theory]
+    [InlineData(10, 20, 10, 20)]
+    [InlineData(0, 0, 0, 0)]
+    [InlineData(-5, -5, -5, -5)]
+    [InlineData(null, null, 0, 0)]
+    public void PositionTest(int? initialX, int? initialY, int expectedX, int expectedY)
     {
         // Arrange
-        const int x = 10;
-        const int y = 20;
-        // Act
-        var positionComponent = new PositionComponent { X = x, Y = y };
-        // Assert
-        Assert.Equal(x, positionComponent.X);
-        Assert.Equal(y, positionComponent.Y);
-    }
+        var component = new PositionComponent();
 
-    [Fact]
-    public void PositionComponent_DefaultInitialization_Works()
-    {
         // Act
-        var positionComponent = new PositionComponent();
-        // Assert
-        Assert.Equal(0.0f, positionComponent.X);
-        Assert.Equal(0.0f, positionComponent.Y);
-    }
+        if(initialX.HasValue) component.X = initialX.Value;
+        if(initialY.HasValue) component.Y = initialY.Value;
 
-    [Fact]
-    public void PositionComponent_SetValues_Works()
-    {
-        // Arrange
-        var positionComponent = new PositionComponent();
-        const int newX = 15;
-        const int newY = 25;
-        // Act
-        positionComponent.X = newX;
-        positionComponent.Y = newY;
         // Assert
-        Assert.Equal(newX, positionComponent.X);
-        Assert.Equal(newY, positionComponent.Y);
+        Assert.Equal(expectedX, component.X);
+        Assert.Equal(expectedY, component.Y);
     }
 }
